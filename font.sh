9d437a66fb5fd9973cbbbffc844ac13ebbe54202
Wesly Tonks <wesly@us01odcvde77874.internal.synopsys.com>
2026-06-12 17:24:03 -0700

    Update font.sh to get the actual nerd font version of jetbrains mono

--- font.sh
+++ font.sh
@@ -1,6 +1,9 @@
 mkdir -p ./fonts
 mkdir -p ~/.local/share/fonts
 cd fonts
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-unzip JetBrainsMono-2.304.zip -d ~/.local/share/
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+tar -xf JetBrainsMono.tar.xz
+mv *.ttf ~/.local/share/fonts
+#wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+#unzip JetBrainsMono-2.304.zip -d ~/.local/share/
 cd ../ && rm -rf fonts
